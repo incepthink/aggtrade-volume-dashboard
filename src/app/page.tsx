14af4554a8715328b9e172b4748c88590b892732
incepthink <incepthink@gmail.com>
2025-12-01 18:00:48 +0530

    fix: header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -162,7 +162,7 @@ export default function VolumePage() {
               mb: 2,
             }}
           >
-            SushiSwap Volume Dashboard
+            AggTrade Volume Dashboard
           </Typography>
 
           <FilterButtons
